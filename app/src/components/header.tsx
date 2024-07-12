890cad9be84f42dc7392a55b14f876e60997437a
robuGdada <paudelronish@gmail.com>
2024-07-12 21:50:31 +0545

    header init

---
+++ app/src/components/header.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import React from "react";
+
+const Header: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <div className="px-[8%] bg-darkVoilet w-full py-8 text-gray-100 flex justify-between items-center fixed">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="cursor-pointer"
+        onClick={() => router.push("/")}
+      >
+        <Image
+          src="/images/healthTrxs.png"
+          alt="header-logo"
+          width={250}
+          height={50}
+        />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="flex gap-4 items-center font-sans font-medium text-white"
+      >
+        <div className="relative cursor-pointer hover:text-gray-200">
+          <span className="underline-effect">About</span>
+        </div>
+        <div className="relative cursor-pointer hover:text-gray-200">
+          <span className="underline-effect">Services</span>
+        </div>
+        <div className="relative cursor-pointer hover:text-gray-200">
+          <span className="underline-effect">Our Products</span>
+        </div>
+        <div className="relative cursor-pointer hover:text-gray-200">
+          <span className="underline-effect">Our Team</span>
+        </div>
+        <div className="relative cursor-pointer hover:text-gray-200">
+          <span className="underline-effect">Contact</span>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export { Header };

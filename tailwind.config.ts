551920e4827aba5f2db3730226473d3252c64972
robuGdada <paudelronish@gmail.com>
2024-07-12 21:49:09 +0545

    font amd color added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["Open Sans", "Roboto", "sans-serif"],
+      },
+      colors: {
+        darkVoilet: "#464862",
+      },
     },
   },
   plugins: [],

997fa45c3dd998483b29dd01a82af27d1b77a172
robuGdada <paudelronish@gmail.com>
2024-07-13 20:23:08 +0545

    footerbanner added

---
+++ app/src/components/footerBanner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FooterBanner = () => {
+  return (
+    <div className="max-w-[1920px] w-full border-t text-center text-[16px] font-sans font-thin gap-10 px-[8%] py-4 bg-blue-950 text-white">
+      © 2024 HealthTrixss LLC. All rights reserved. Designed & Developed by
+      Technopoets Pvt. ltd
+    </div>
+  );
+};
+
+export default FooterBanner;

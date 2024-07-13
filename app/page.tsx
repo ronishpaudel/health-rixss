3abd3e81b2cdd0ee1f49744bdaf31d0a47a2bb54
robuGdada <paudelronish@gmail.com>
2024-07-13 21:25:50 +0545

    loading add

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Header } from "./src/components/header";
 import { Main } from "./src/components/main";
 import { Footer } from "./src/components/footer";
@@ -6,17 +6,20 @@ import { Testimonials } from "./src/components/testimonial";
 import { ClientTestimonials } from "./src/components/client";
 import FooterBanner from "./src/components/footerBanner";
 import ScrollToTopButton from "./src/components/scrollTopButton";
+import Loading from "./loading";
 
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center ">
-      <Header />
-      <Main />
-      <Testimonials />
-      <ClientTestimonials />
-      <Footer />
-      {/* <FooterBanner /> */}
-      <ScrollToTopButton />
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <Main />
+        <Testimonials />
+        <ClientTestimonials />
+        <Footer />
+        {/* <FooterBanner /> */}
+        <ScrollToTopButton />
+      </Suspense>
     </div>
   );
 };

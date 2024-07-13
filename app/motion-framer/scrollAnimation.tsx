49a8b2845d40a4b13d047fb32f75acc65a765e43
robuGdada <paudelronish@gmail.com>
2024-07-13 11:07:47 +0545

    scrool animation implement

---
+++ app/motion-framer/scrollAnimation.tsx
@@ -0,0 +1,61 @@
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+export interface scrollAnimationProps {
+  children: JSX.Element;
+  className?: string;
+}
+
+const ScrollAnimation = ({ children, className }: scrollAnimationProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div
+      ref={ref}
+      style={{ position: "relative", overflow: "hidden" }}
+      className={className}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          zIndex: 20,
+        }}
+      />
+    </div>
+  );
+};
+
+export { ScrollAnimation };

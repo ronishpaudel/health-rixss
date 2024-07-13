6a15a5ee82f9832b3305b8b7e107a46e42f96b62
robuGdada <paudelronish@gmail.com>
2024-07-13 20:23:15 +0545

    header ui and navbar fix

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -19,49 +19,66 @@ const Header: React.FC = () => {
       <div className="px-[8%] max-w-[1920px] w-full bg-darkVoilet py-8">
         <div className="text-gray-100 flex justify-between items-center w-full">
           <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
             className="cursor-pointer"
             onClick={() => router.push("/")}
           >
             <Image
               src="/images/healthTrxs.png"
               alt="header-logo"
-              width={250}
               height={50}
+              width={230}
             />
           </motion.div>
 
           <div className="bg-blue-500 p-1 rounded md:hidden block">
             {!navbar ? (
-              <Menu size={25} onClick={onClickSetNavbar} />
+              <Menu size={23} onClick={onClickSetNavbar} />
             ) : (
-              <X onClick={onClickSetNavbar} />
+              <X size={23} onClick={onClickSetNavbar} />
             )}
           </div>
 
           <div className="md:block hidden">
-            <motion.div
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-              className="flex gap-4 items-center font-sans font-medium text-white"
-            >
+            <motion.div className="flex gap-4 items-center font-sans font-medium text-white">
               <div className="relative cursor-pointer hover:text-gray-200">
-                <span className="underline-effect">About</span>
+                <span
+                  className="underline-effect"
+                  onClick={() => router.push("/about-us")}
+                >
+                  About
+                </span>
               </div>
               <div className="relative cursor-pointer hover:text-gray-200">
-                <span className="underline-effect">Services</span>
+                <span
+                  className="underline-effect"
+                  onClick={() => router.push("/services")}
+                >
+                  Services
+                </span>
               </div>
               <div className="relative cursor-pointer hover:text-gray-200">
-                <span className="underline-effect">Our Products</span>
+                <span
+                  className="underline-effect"
+                  onClick={() => router.push("/our-products")}
+                >
+                  Our Products
+                </span>
               </div>
               <div className="relative cursor-pointer hover:text-gray-200">
-                <span className="underline-effect">Our Team</span>
+                <span
+                  className="underline-effect"
+                  onClick={() => router.push("/our-team")}
+                >
+                  Our Team
+                </span>
               </div>
               <div className="relative cursor-pointer hover:text-gray-200">
-                <span className="underline-effect">Contact</span>
+                <span
+                  className="underline-effect"
+                  onClick={() => router.push("/contact-us")}
+                >
+                  Contact
+                </span>
               </div>
             </motion.div>
           </div>

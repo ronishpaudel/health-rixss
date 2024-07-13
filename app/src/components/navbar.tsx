f95beb6febd53748c38c1ebe4929fd6387ccdf43
robuGdada <paudelronish@gmail.com>
2024-07-13 20:23:40 +0545

    navbar iui fix

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -1,22 +1,37 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NavBar = () => {
+  const { push } = useRouter();
   return (
     <div className=" flex flex-col items-center space-y-2 bg-white text-darkVoilet">
       <div className="relative cursor-pointer hover:text-gray-200 w-full border-b border-gray-300 py-2">
-        <span className="underline-effect">About</span>
+        <span className="underline-effect" onClick={() => push("/about-us")}>
+          About
+        </span>
       </div>
       <div className="relative cursor-pointer hover:text-gray-200 w-full border-b border-gray-300 py-2">
-        <span className="underline-effect">Services</span>
+        <span className="underline-effect" onClick={() => push("/services")}>
+          Services
+        </span>
       </div>
       <div className="relative cursor-pointer hover:text-gray-200 w-full border-b border-gray-300 py-2">
-        <span className="underline-effect">Our Products</span>
+        <span
+          className="underline-effect"
+          onClick={() => push("/our-products")}
+        >
+          Our Products
+        </span>
       </div>
       <div className="relative cursor-pointer hover:text-gray-200 w-full border-b border-gray-300 py-2">
-        <span className="underline-effect">Our Team</span>
+        <span className="underline-effect" onClick={() => push("/our-team")}>
+          Our Team
+        </span>
       </div>
       <div className="relative cursor-pointer hover:text-gray-200 w-full border-b border-gray-300 py-2">
-        <span className="underline-effect">Contact</span>
+        <span className="underline-effect" onClick={() => push("/contact-us")}>
+          Contact
+        </span>
       </div>
     </div>
   );

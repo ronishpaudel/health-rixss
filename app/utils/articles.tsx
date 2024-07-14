25b2ef82defd66d61a657b70ea32c79802c862ec
robuGdada <paudelronish@gmail.com>
2024-07-14 14:49:42 +0545

    dummy data for articles add

---
+++ app/utils/articles.tsx
@@ -0,0 +1,35 @@
+export const articles = [
+  {
+    image: "/newsletter/decison.jpg",
+    alt: "News Article 6",
+    title: "Leveraging Data for Informed Decisions",
+    description: `Discover how data-driven insights can transform your business strategy.
+    Explore the latest advancements in health data analytics and how they are 
+    improving patient outcomes.`,
+    id: "article-1", // String ID
+  },
+  {
+    image: "/newsletter/bigdata.jpeg",
+    alt: "News Article 7",
+    title: "Advancements in Health Data Analytics",
+    description:
+      "Explore the latest advancements in health data analytics and how they are improving patient outcomes.",
+    id: "article-2", // String ID
+  },
+  {
+    image: "/newsletter/healthcare.jpeg",
+    alt: "News Article 8",
+    title: "The Role of AI in Healthcare",
+    description:
+      "Learn about the role of artificial intelligence in transforming healthcare delivery and patient care.",
+    id: "article-3", // String ID
+  },
+  {
+    image: "/newsletter/telehealth.jpeg",
+    alt: "News Article 9",
+    title: "Telehealth: The Future of Healthcare",
+    description:
+      "Understand the benefits of telehealth and how it is shaping the future of healthcare.",
+    id: "article-4", // String ID
+  },
+];

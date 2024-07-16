73e4e1c54e6d424fd39da3c725b6128e145cf47b
robuGdada <paudelronish@gmail.com>
2024-07-16 20:52:06 +0545

    link and creator name fix

--- app/src/components/footerBanner.tsx
+++ app/src/components/footerBanner.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const FooterBanner = () => {
   return (
     <div className="max-w-[1920px] w-full border-t text-center text-[16px] font-sans font-thin gap-10 px-[8%] py-4 bg-blue-950 text-white">
-      © 2024 HealthTrixss LLC. All rights reserved. Designed & Developed by
-      ronishpaudel.com.np
+      © 2024 HealthTrixss LLC. All rights reserved. Designed & Developed by{" "}
+      <Link
+        href="https://www.ronishpaudel.com.np/"
+        className="text-orange-400 hover:text-orange-500 hover:shadow-xl"
+      >
+        ronishpaudel.com.np
+      </Link>
     </div>
   );
 };

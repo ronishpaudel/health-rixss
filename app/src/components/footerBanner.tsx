a4f35a91c8109bd3dadb763c7214852485846e0b
robuGdada <paudelronish@gmail.com>
2024-07-16 20:43:52 +0545

    creator name fix

--- app/src/components/footerBanner.tsx
+++ app/src/components/footerBanner.tsx
@@ -4,7 +4,7 @@ const FooterBanner = () => {
   return (
     <div className="max-w-[1920px] w-full border-t text-center text-[16px] font-sans font-thin gap-10 px-[8%] py-4 bg-blue-950 text-white">
       © 2024 HealthTrixss LLC. All rights reserved. Designed & Developed by
-      Technopoets Pvt. ltd
+      ronishpaudel.com.np
     </div>
   );
 };
